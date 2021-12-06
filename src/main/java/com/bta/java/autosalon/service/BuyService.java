83b7d50ff88bcbc381e062bbb230d72f3c9c4118
oleg.pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2021-12-06 20:23:09 +0200

    added Buy Methods

--- src/main/java/com/bta/java/autosalon/service/BuyService.java
+++ src/main/java/com/bta/java/autosalon/service/BuyService.java
@@ -1,14 +1,71 @@
 package com.bta.java.autosalon.service;
 
+import com.bta.java.autosalon.model.Balance;
+import com.bta.java.autosalon.model.Place;
 import com.bta.java.autosalon.model.car.Car;
+import com.bta.java.autosalon.repository.BalanceRepository;
+import com.bta.java.autosalon.repository.CarRepository;
+import com.bta.java.autosalon.repository.PlaceRepository;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BuyService {
-  public void evaluatePrice(Car carToBuy, long clientPrice) {
 
+  @Autowired
+  private ParkingPlaceValidator parkingPlaceValidator;
+
+  @Autowired
+  private BalanceRepository balanceRepository;
+
+  @Autowired
+  private CarRepository carRepository;
+
+  @Autowired
+  private PlaceRepository placeRepository;
+
+  public boolean evaluatePrice(Car carToBuy, long clientPrice) {
+    final long salonPrice = evaluateCar(carToBuy);
+    final long priceDifference = (clientPrice - salonPrice) * 100 / salonPrice;
+
+    return priceDifference <= 15;
+  }
+
+  public void buyCar(Car carToBuy, long price) {
+    if (!parkingPlaceValidator.hasFreePlace()) {
+      System.out.println("No free parking places");
+      return;
+    }
+    long balance = balanceRepository.findAllOrderedByTransactionTime().stream()
+        .findFirst().get().getBalance();
+    if (price > balance) {
+      System.out.println("No sufficient balance");
+      return;
+    }
+    carRepository.save(carToBuy);
+    parkCarAfterBuy(carToBuy);
+    applyTx(carToBuy, balance, price);
+  }
+
+  private void parkCarAfterBuy(Car car) {
+    final int freePlace = parkingPlaceValidator.getFreePlace();
+    Place place = Place.builder()
+        .number(freePlace)
+        .car(car)
+        .build();
+    placeRepository.save(place);
+  }
+
+  private void applyTx(Car car, Long currentBalance, Long price) {
+    long newBalance = currentBalance - price;
+    Balance buyTx = Balance.builder()
+        .summ(-price)
+        .balance(newBalance)
+        .car(car)
+        .build();
+    balanceRepository.save(buyTx);
   }
 
   public long evaluateCar(Car car) {
